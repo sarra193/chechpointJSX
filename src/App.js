a13c3614437588829d62d36add086510bee5bcf5
sarra193 <slimensarra0@gmail.com>
2021-01-17 22:55:34 +0100

    updating css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './style.css';
-import logo from './assets/logo192.png';
+import bg from './assets/bg.jpg'
+import pic from './assets/pic2.jpg'
 
 function App() {
   return (
-    <div className="App">
-    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
-          <h1 className="title red"> JSX Checkpoint</h1>
+    <div className="App" style={{backgroundImage: `url(${bg})`}}>
+    <div  style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red Row"> JSX Checkpoint</h1>
           <br />
-          <img src={logo} alt=""/>
+          <img className="img" src={pic} alt=""/>
           <br />
-          <img src="/logo192.png" alt="" />
+          <img className="img" src="/pic1.jpg" alt="" roundedCircle />
         </div>
         <video controls width={320} height={240}>
           <source src="myVideo.mp4" type="video/mp4" />
