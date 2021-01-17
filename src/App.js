542bf909a4f777bafeec262f296d13b3251a1c94
sarra193 <slimensarra0@gmail.com>
2021-01-17 20:24:08 +0100

    Convert html to jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo192.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">React Project</h1>
+          <br />
+          <img src={logo} alt=""/>
+          <br />
+          <img src="/logo192.png" alt="" />
+        </div>
+        <video controls width={320} height={240}>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
     </div>
   );
 }
